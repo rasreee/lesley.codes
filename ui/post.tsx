f80b05aa1127882fa809e676b7c59dd67dc355ba
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-22 21:34:39 -0500

    post callout

--- ui/post.tsx
+++ ui/post.tsx
@@ -36,8 +36,11 @@ const PostHeader = ({ title, image, publishedAt }: PostHeaderProps) => {
 
 const PostCallout = ({ children }) => {
   return (
-    <div className="bg-cultured dark:bg-gray-800 rounded-lg w-full p-6 max-w-2xl mx-auto">
-      <div className="text markdown align-middle">{children}</div>
+    <div className="bg-cultured dark:bg-gray-800 rounded w-full px-4 py-4 max-w-2xl mx-auto">
+      <div className="flex items-start gap-3">
+        <span className="text-2xl">{'💡'}</span>
+        <div className="text markdown align-middle">{children}</div>
+      </div>
     </div>
   )
 }
