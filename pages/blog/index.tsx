0a98a9a3df261bae5d0a6f326134113deb249bf8
Lesley Chang <lesley.hanna98@gmail.com>
2021-12-22 22:21:23 -0500

    asdf

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -19,19 +19,23 @@ export default function Blog({ allPosts }: Props) {
       description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
     >
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
-        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">Blog</h1>
-        <p className="mb-4 text-gray-600 dark:text-gray-400">
-          {`I've been writing online since 2014, mostly about web development and tech careers.
+        <div className="mb-4">
+          <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">Blog</h1>
+        </div>
+        <div className="mb-4">
+          <p className="mb-4 text-gray-600 dark:text-gray-400 text-body">
+            {`I've been writing online since 2014, mostly about web development and tech careers.
             In total, I've written ${allPosts.length} articles on my blog.
             Use the search below to filter by title.`}
-        </p>
+          </p>
+        </div>
         <div className="relative w-full mb-4">
           <input
             aria-label="Search articles"
             type="text"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder="Search articles"
-            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800  dark:text-gray-100"
           />
           <svg
             className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
