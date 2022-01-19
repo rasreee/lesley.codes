d6328140453a5240fcada4770012767e301fd428
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-19 04:41:10 -0500

    fix

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,44 +1,16 @@
 import Layout from '@components/Layout';
-import { SearchablePostFeed } from '@components/SearchablePostFeed';
 import { Section } from '@components/Section';
-import { H1, P } from '@components/Typography';
-import { getAllPosts } from '@lib/api';
+import { H1 } from '@components/Typography';
 import { AppConfig } from '@lib/appConfig';
-import { BlogFrontmatterWithSlug } from '@lib/frontmatter';
-import NextLink from '@ui/components/NextLink';
-import RightArrowIcon from '@ui/icons/RightArrowIcon';
-import { GetStaticProps } from 'next';
 
-type IndexProps = {
-  posts: BlogFrontmatterWithSlug[];
-};
-
-const Index = ({ posts }: IndexProps): JSX.Element => {
+const Playground = () => {
   return (
     <Layout meta={AppConfig.meta}>
       <Section>
-        <H1>{AppConfig.meta.title}</H1>
-        <P>{AppConfig.meta.description}</P>
-      </Section>
-      <Section>
-        <SearchablePostFeed posts={posts} />
-      </Section>
-      <Section>
-        <NextLink href="/blog" className="flex mt-8">
-          Read all posts
-          <RightArrowIcon />
-        </NextLink>
+        <H1>{'Dev Playground'}</H1>
       </Section>
     </Layout>
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = getAllPosts();
-
-  return {
-    props: { posts },
-  };
-};
-
-export default Index;
+export default Playground;
