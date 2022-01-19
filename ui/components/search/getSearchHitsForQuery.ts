d4fb955f5f5bd14d2bfc67846dd97607caea3b32
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-19 07:08:40 -0500

    clean

--- ui/components/search/getSearchHitsForQuery.ts
+++ ui/components/search/getSearchHitsForQuery.ts
@@ -4,17 +4,17 @@ import { SearchHit } from './SearchHit';
 const allData = mockSearchHits;
 
 export function getSearchHitsForQuery(query: string): Promise<SearchHit[]> {
+  if (!query) return Promise.resolve([]);
+
   let hits = [] as SearchHit[];
 
-  if (query.length) {
-    const a = query.toLowerCase();
+  const a = query.toLowerCase();
 
-    hits = allData.filter((item) => {
-      const b = item.title.toLowerCase().slice(0, query.length);
+  hits = allData.filter((item) => {
+    const b = item.title.toLowerCase().slice(0, query.length);
 
-      return a === b;
-    });
-  }
+    return a === b;
+  });
 
   return Promise.resolve(hits);
 }
