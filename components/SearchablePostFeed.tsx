9d347f5888e080b4c79529d1ed72b9f9cd29e6e1
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-19 12:43:21 -0500

    sort posts with newest at the top

--- components/SearchablePostFeed.tsx
+++ components/SearchablePostFeed.tsx
@@ -1,6 +1,7 @@
 import { BlogFrontmatterWithSlug } from '@lib/frontmatter';
 import { useDebouncedState } from '@ui/hooks/useDebouncedState';
 import SearchIcon from '@ui/icons/SearchIcon';
+import isBefore from 'date-fns/isBefore';
 import isPast from 'date-fns/isPast';
 import parseISO from 'date-fns/parseISO';
 import React, { useMemo } from 'react';
@@ -17,11 +18,12 @@ interface SearchablePostFeedProps {
 export const SearchablePostFeed: React.FunctionComponent<SearchablePostFeedProps> = ({ posts }) => {
   const [query, setQuery] = useDebouncedState(``, 300);
 
-  const filteredBlogPosts = useMemo(
+  const sortedAndFilteredPosts = useMemo(
     () =>
       posts
         .filter((post) => post.title.toLowerCase().includes(query.toLowerCase()))
-        .filter((post) => isPast(parseISO(post.publishedAt))),
+        .filter((post) => isPast(parseISO(post.publishedAt)))
+        .sort((a, b) => (isBefore(parseISO(a.publishedAt), parseISO(b.publishedAt)) ? 1 : -1)),
     [posts, query]
   );
 
@@ -31,9 +33,9 @@ export const SearchablePostFeed: React.FunctionComponent<SearchablePostFeedProps
         <WideSearchInput query={query} onChange={setQuery} />
         <SearchIcon className="absolute right-3 top-[1.125rem]" />
       </Section>
-      {!filteredBlogPosts.length && <P>No posts found.</P>}
+      {!sortedAndFilteredPosts.length && <P>No posts found.</P>}
       <Section>
-        <PostFeed posts={filteredBlogPosts} className="gap-10" />
+        <PostFeed posts={sortedAndFilteredPosts} className="gap-10" />
       </Section>
     </>
   );
