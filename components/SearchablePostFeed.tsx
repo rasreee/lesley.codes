f7df15c5e4d396ab326f421016042a552d65f2f0
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-18 22:21:44 -0500

    clean

--- components/SearchablePostFeed.tsx
+++ components/SearchablePostFeed.tsx
@@ -1,70 +1,14 @@
 import { isPast, parseISO } from 'date-fns';
 import { useAutoFocus } from 'hooks/useAutoFocus';
 import { useDebouncedState } from 'hooks/useDebouncedState';
-import { cn } from 'lib/classnames';
 import { BlogFrontmatterWithSlug } from 'lib/frontmatter';
-import React, {
-  ChangeEventHandler,
-  forwardRef,
-  InputHTMLAttributes,
-  Ref,
-  SVGProps,
-  useEffect,
-  useMemo,
-  useRef,
-} from 'react';
+import React, { ChangeEventHandler, useEffect, useMemo, useRef } from 'react';
 
 import { PostFeed } from './PostFeed';
+import { SearchIcon } from './SearchIcon';
+import { SearchInput } from './SearchInput';
 import { Section } from './Section';
 import { P } from './Typography';
-
-const SearchIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
-  return (
-    <svg
-      {...props}
-      className={cn(className, 'w-5 h-5 text-gray-400 dark:text-gray-300')}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-      />
-    </svg>
-  );
-};
-
-const SearchInput = forwardRef(
-  (
-    props: InputHTMLAttributes<HTMLInputElement>,
-    ref: Ref<HTMLInputElement>
-  ) => {
-    return (
-      <input
-        ref={ref}
-        aria-label="Search articles"
-        type="text"
-        placeholder="Search articles"
-        className={cn(
-          'bg-white dark:bg-gray-800',
-          'border border-gray-200 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500',
-          'w-full',
-          'text',
-          'rounded-md',
-          'px-4 py-2'
-        )}
-        {...props}
-      />
-    );
-  }
-);
-
-SearchInput.displayName = `SearchInput`;
-
 interface SearchablePostFeedProps {
   posts: BlogFrontmatterWithSlug[];
 }
