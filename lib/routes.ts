0dc333c1eda0e4063f95bcf5b40322309404818b
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-19 05:06:03 -0500

    clean

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,5 +1,4 @@
 import { isProduction } from './environment';
-import { getBasePath } from './utils/getBasePath';
 
 const DefaultRoutes = [
   {
@@ -20,21 +19,4 @@ const DevRoutes = [
   },
 ];
 
-const Routes = isProduction() ? DefaultRoutes : DevRoutes;
-
-export { Routes };
-
-export const getRouteTitle = (pathname: string): string => {
-  const basePath = getBasePath(pathname);
-  const pageTitle = Routes.find((route) => route.path === basePath)?.title;
-  if (!pageTitle) {
-    const error = new Error(
-      `could not find route title for pathname=${pathname} and basePath=${basePath}`
-    );
-    console.error(error);
-    console.log('Routes: ', JSON.stringify(Routes, null, 4));
-    throw error;
-  }
-
-  return pageTitle;
-};
+export const Routes = isProduction() ? DefaultRoutes : DevRoutes;
