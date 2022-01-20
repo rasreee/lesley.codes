eb2c41620a030501927d4d4d81f4690a8d988ed4
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-20 04:11:00 -0500

    wip

--- ui/styles/GlobalStyles.tsx
+++ ui/styles/GlobalStyles.tsx
@@ -10,6 +10,8 @@ const globalStyles = css`
     box-sizing: border-box;
     -moz-osx-font-smoothing: grayscale;
     -webkit-font-smoothing: antialiased;
+    margin: 0;
+    padding: 0;
   }
 
   * {
@@ -27,17 +29,8 @@ const globalStyles = css`
   body {
     font-size: 16px;
     line-height: 150%;
-    margin: 0;
-    padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
       'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    scroll-behavior: smooth;
-    transition-property: all;
-    transition-duration: 150ms;
-    transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
-    min-height: 100%;
   }
 
   #__next {
