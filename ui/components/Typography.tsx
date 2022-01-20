f9969ae117c8f4bdd134fcb78ed7de38af49dadc
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-20 04:24:40 -0500

    migrate Typography to emotion

--- ui/components/Typography.tsx
+++ ui/components/Typography.tsx
@@ -2,18 +2,6 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { largerThan } from '@ui/utils/breakpoints';
 import { StyledProps } from '@ui/utils/emotion';
-import classNames from 'classnames';
-import { HTMLAttributes, ReactNode } from 'react';
-
-export interface TextProps extends Omit<HTMLAttributes<HTMLElement>, 'color'> {
-  children: ReactNode;
-  /* Custom text color className */
-  color?: string;
-  /* Custom font size className */
-  size?: string;
-  /* Custom font weight className */
-  weight?: string;
-}
 
 const headingStyles = ({ theme }: StyledProps) => css`
   font-weight: ${theme.fontWeights.bold};
@@ -68,30 +56,15 @@ export const P = styled.p`
   `}
 `;
 
-const defaultTextColor = 'text-gray-700 dark:text-gray-100';
-
-export const Span: React.FC<TextProps> = ({
-  children,
-  className,
-  size = 'text-base md:text-lg',
-  color = defaultTextColor,
-  weight = 'font-normal',
-  ...props
-}) => {
-  return (
-    <span
-      className={classNames(
-        color,
-        size,
-        weight,
-        className
-          ?.split(' ')
-          .map((c) => `${c}`)
-          .join(' ')
-      )}
-      {...props}
-    >
-      {children}
-    </span>
-  );
-};
+export const Span = styled.p`
+  ${bodyStyles}
+  ${({ theme }) => css`
+    font-size: ${theme.fontSizes['base']};
+    ${largerThan('tablet')} {
+      font-size: ${theme.fontSizes['lg']};
+    }
+    ${largerThan('desktop')} {
+      font-size: ${theme.fontSizes['2xl']};
+    }
+  `}
+`;
